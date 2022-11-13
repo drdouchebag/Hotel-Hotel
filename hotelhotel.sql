1dd45da352df0bd72792c7cd4fb47cd99eee1934
Doc <79391282+healthycheesecake@users.noreply.github.com>
2022-11-13 20:39:07 +0530

    Update hotelhotel.sql

--- hotelhotel.sql
+++ hotelhotel.sql
@@ -1,1 +1,8 @@
+create database Hotel Hotel;
+use Hotel Hotel;
+create table customers(c_id int, Full_name varchar(255), Address varchar(255), Phone_no int, Room_id int, Check_in varchar(255), Check_out varchar(255), no_of_people int);
+create table employee(emp_id int, Full_name varchar(255), Address varchar(255), Phone_no int, Working_hours int, Salary int, Department varchar(255));
+create table rooms(room_type varchar(255),room_id int,description varchar(255));
+create table services(s_no int,s_name varchar(255),emp_id int);
+create table restaurant(emp_id int, hierarchy varchar(255),name varchar(255));
 
