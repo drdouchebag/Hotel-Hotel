b1ebc038b21d95bba6b578613d472e3d941eec1e
simplyrogue <118063632+simplyrogue@users.noreply.github.com>
2022-12-05 21:46:13 +0530

    Update hotelhotel.py

--- hotelhotel.py
+++ hotelhotel.py
@@ -1,5 +1,22 @@
 import mysql.connector
 myconn = mysql.connector.connect(host = 'localhost', user = 'root', password = '1234', database = 'hotel_hotel')
 cursor_obj = connection_object.cursor()
-cursor_obj.execute('')
-  
+
+#for table customers
+c_id = input('Enter the customer ID: ')
+name = input('Enter the customer full name: ')
+address = input('Enter the primary address: ')
+phnumber = input("Enter the customer's phone number: ")
+room_id = input('Enter the room number of the allotted suite: ')
+check_in = input('Enter the check-in date (YYYY/MM/DD): ')
+check_out = input('Enter the check-out date (YYYY/MM/DD): ')
+peopleno = input('Enter the number of people: ')
+
+cursor_obj.execute('INSERT INTO customers VALUES({}{}{}{}{}{}{}{}'.format(c_id,name,address,phnumber,room_id,check_in,check_out,peopleno))
+
+#for table employee
+emp_id = input('Enter the employee ID: ')
+e_name = input('Enter the employee full name: ')
+e_address = input('Enter the primary address: ')
+e_phnumber = input("Enter the employee's phone number: ")
+Working hours = input('Enter the number of hours he works: ')
