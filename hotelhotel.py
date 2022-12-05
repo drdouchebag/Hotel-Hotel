e4bc278813ccdf8a2633c79870fa17550effeb0f
simplyrogue <118063632+simplyrogue@users.noreply.github.com>
2022-12-05 21:27:29 +0530

    Update hotelhotel.py

--- hotelhotel.py
+++ hotelhotel.py
@@ -1,1 +1,5 @@
-
+import mysql.connector
+myconn = mysql.connector.connect(host = 'localhost', user = 'root', password = '1234', database = 'hotel_hotel')
+cursor_obj = connection_object.cursor()
+cursor_obj.execute('')
+  
