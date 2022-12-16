9bdc6e951616a55e41609971eebfd5e05c8c3d9f
simplyrogue <118063632+simplyrogue@users.noreply.github.com>
2022-12-16 13:47:45 +0530

    Update hotelhotel.sql

--- hotelhotel.sql
+++ hotelhotel.sql
@@ -1,10 +1,28 @@
 CREATE DATABASE Hotel_Hotel;
 use Hotel_Hotel;
 /*table stating the details of all customers*/
-CREATE TABLE customers(c_id INT, Full_name VARCHAR(255), Address VARCHAR(255), Phone_no INT, room_no INT, Check_in VARCHAR(255), Check_out VARCHAR(255), no_of_people INT, Service_opted_1 VARCHAR(50) DEFAULT 'NULL', Service_opted_2 VARCHAR(50) DEFAULT 'NULL', Service_opted_3 VARCHAR(50) DEFAULT 'NULL');
-CREATE TABLE employee(emp_id INT, Full_name varchar(255), Address VARCHAR(255), Phone_no INT, Working_hours INT, Salary INT, Department VARCHAR(255));
+CREATE TABLE customers(c_id INT PRIMARY KEY, 
+                       Full_name VARCHAR(255), 
+                       Address VARCHAR(255), 
+                       Phone_no VARCHAR(255), 
+                       room_no INT, 
+                       Check_in DATE(255), 
+                       Check_out DATE(255), 
+                       no_of_people INT, 
+                       );
+CREATE TABLE employee(emp_id INT PRIMARY KEY, 
+                      Full_name varchar(255), 
+                      Address VARCHAR(255), 
+                      Phone_no VARCHAR(255), 
+                      Working_hours INT, 
+                      Salary INT, 
+                      Department VARCHAR(255), 
+                      Position VARCHAR(255));
 
-CREATE TABLE roomtypes(room_id INT PRIMARY KEY, room_type VARCHAR(255), description VARCHAR(255), rent VARCHAR(10));
+CREATE TABLE roomtypes(room_id INT PRIMARY KEY, 
+                       room_type VARCHAR(255), 
+                       Description VARCHAR(255), 
+                       Rent VARCHAR(10));
 INSERT INTO roomtypes VALUES(101, 'Standard Suite Room', 'Living room connected to 2 bedrooms w/ 2 bathrooms', '$550');
 INSERT INTO roomtypes VALUES(102, 'Junior Suite Room', 'Living room connected to a bedroom w/ 1 bathroom', '$350');
 INSERT INTO roomtypes VALUES(103, 'Presidential Suite Room', 'Luxurious living space connected to 2 bedrooms, 2 bathrooms, private balcony & unlimited access to entertainment area', '$1750');
@@ -12,7 +30,11 @@ INSERT INTO roomtypes VALUES(104, 'Honeymoon Suite room', 'Designed for couples;
 
 
 /*table stating the availability of all 18 rooms in the hotel */
-CREATE TABLE rooms(room_no VARCHAR(50), floor VARCHAR(50), room_id INT, Availability VARCHAR(50));
+CREATE TABLE rooms(room_no VARCHAR(50), 
+                   floor VARCHAR(50), 
+                   room_id INT, 
+                   Availability VARCHAR(50));
+
 /* 6 standard suite rooms */ 
 INSERT INTO rooms VALUES('S01', 'Floor 2' , 101 , 'VACANT');
 INSERT INTO rooms VALUES('S02', 'Floor 2' , 101 , 'VACANT');
